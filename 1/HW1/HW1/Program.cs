23d2704f862825677874a6bc5620a4f2acab000b
Богдан Драгузя <draguziabogdan@gmail.com>
2018-06-01 00:51:17 +0300

    first commit

---
+++ 1/HW1/HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Bogdan");
+            Console.ResetColor();
+
+            Console.ReadKey();
+        }
+    }
+}
