c199e22310ecf87c0f5b19696e088a6e86af4459
Богдан Драгузя <draguziabogdan@gmail.com>
2018-06-01 02:04:22 +0300

    Want to sleep

---
+++ 2/HW2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String num1, num2;
+            num1 = "";num2 = "abc";
+            num1 += num2;
+            ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+            while ( keyInfo.Key!=ConsoleKey.Enter)
+            {
+                //keyInfo = new ConsoleKeyInfo();
+                Console.WriteLine("Iteration");
+            }
+            Double.TryParse("123,45", out double result);
+
+            Console.WriteLine(result*2);
+            
+            Console.ReadKey();
+        }
+    }
+}
