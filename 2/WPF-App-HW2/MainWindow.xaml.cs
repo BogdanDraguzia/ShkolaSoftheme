aa0b794cc228a037a511efed3eb5a85761778731
Богдан Драгузя <draguziabogdan@gmail.com>
2018-06-02 16:27:36 +0300

    HW2 done

---
+++ 2/WPF-App-HW2/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using static System.Double;
+
+namespace WPF_App_HW2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBlock1.Text+="byte:(" +byte.MinValue +","+byte.MaxValue+")";
+            TextBlock1.Text += "\nshort:(" + short.MinValue + "," + short.MaxValue + ")";
+            TextBlock1.Text += "\nint:(" + int.MinValue + "," + int.MaxValue + ")";
+            TextBlock1.Text += "\nlong:(" + long.MinValue + "," + long.MaxValue + ")";
+            TextBlock1.Text += "\ndecimal:(" + decimal.MinValue + "," + decimal.MaxValue + ")";
+            TextBlock1.Text += "\nfloat:(" + float.MinValue + "," + float.MaxValue + ")";
+            TextBlock1.Text += "\ndouble:(" + MinValue + "," + MaxValue + ")";
+
+        }
+
+    }
+}
