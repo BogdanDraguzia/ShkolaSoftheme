f5e0f569daee47906f401fb2299221563eb824d6
Богдан Драгузя <draguziabogdan@gmail.com>
2018-06-10 03:22:36 +0300

    distinct element found

---
+++ 10/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int?[] array = new int?[1001];
+
+        private static void Main(string[] args)
+        {
+            Initialize();
+            var distinct = GetDistinct();
+            Console.WriteLine(distinct.ToString());
+
+            Console.ReadKey();
+        }
+
+        private static int? GetDistinct()
+        {
+            int? distinct = null;
+            for (var i = 0; i < 1001; i++)
+            {
+                var tmp = array[i]; //temporarily remove current item and then puts it back
+                array[i] = null;
+                if (!array.Contains(tmp))
+                {
+                    distinct = tmp;
+                    break;
+                }
+
+                array[i] = tmp; //returning back to array
+            }
+
+            return distinct;
+        }
+
+        private static void Initialize()
+        {
+            for (var i = 0; i < 500; i++)
+            {
+                array[i] = i;
+                array[i + 500] = i;
+                array[1000] = 1000;
+            }
+        }
+    }
+}
