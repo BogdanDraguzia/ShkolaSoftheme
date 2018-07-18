84f5f1e7d3fd1e1afee269d134d6361e21a5729f
Богдан Драгузя <draguziabogdan@gmail.com>
2018-07-18 20:39:11 +0300

    HW-17 Done

---
+++ 17/Iterator/ConcreteAggregate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Iterator
+{
+    class ConcreteAggregate<T> : Aggregate<T>
+    {
+        private readonly T[] _items = new T[20]; //to do: Resize when full
+
+        public override Iterator<T> CreateIterator()
+        {
+            return new ConcreteIterator<T>(this);
+        }
+
+        public int Count { get; private set; }
+
+        private void Insert(int index, T value)
+        {
+            _items[index] = value;
+            Count++;
+        }
+
+        public object this[int index]
+        {
+            get { return _items[index]; }
+            set { Insert(index, (T)value); }
+        }
+    }
+}
---
+++ 17/Iterator/Iterator.cs
@@ -0,0 +1,13 @@
+﻿namespace Iterator
+{
+    abstract class Iterator<T>
+    {
+        public abstract object First();
+        
+        public abstract object Next();
+        
+        public abstract bool IsDone();
+        
+        public abstract object CurrentItem();
+    }
+}
